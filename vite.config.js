0006de9a63e7f3074c2fc97dad84fd8294039b4c
William Queiroz de Araujo <williamq2006@gmail.com>
2025-10-05 15:28:12 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CP4-portfolio/',
+  base: '/Landing-page/',
+  build: {
+    outDir: 'docs', 
+  },
 })
